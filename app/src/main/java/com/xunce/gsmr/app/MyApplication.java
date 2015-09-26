5dab418fe6eca3f6b42f1c6266997c32f2ae2267
Jason <jeson.g@gmail.com>
2015-09-26 20:29:12 +0800

    加入BugHD统计

--- app/src/main/java/com/xunce/gsmr/app/MyApplication.java
+++ app/src/main/java/com/xunce/gsmr/app/MyApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.activeandroid.ActiveAndroid;
 import com.baidu.mapapi.SDKInitializer;
 
+import im.fir.sdk.FIR;
+
 /**
  * 程序入口
  * Created by ssthouse on 2015/7/16.
@@ -13,6 +15,8 @@ public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
+        FIR.init(this);
+
         super.onCreate();
         //百度地图初始化
         SDKInitializer.initialize(getApplicationContext());
